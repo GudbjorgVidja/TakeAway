ecfe8248b08c6bc07b2c8369706cce677939cc8a
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-14 08:43:36 +0000

    Made test cases for Vidskiptavinur

--- src/test/java/vinnsla/takeaway/VidskiptavinurTest.java
+++ src/test/java/vinnsla/takeaway/VidskiptavinurTest.java
@@ -1,4 +1,31 @@
 package vinnsla.takeaway;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class VidskiptavinurTest {
+    private Vidskiptavinur vidskiptavinur;
+    private String nafn;
+    private String heimilisfang;
+
+    @Before
+    public void makeVidskiptavinur() {
+        nafn = "Jónatan";
+        heimilisfang = "Birkiteigur 15";
+        vidskiptavinur = new Vidskiptavinur(nafn, heimilisfang);
+    }
+    
+
+    @Test
+    public void testNafnIsName() {
+        assertEquals(vidskiptavinur.getNafn().get(), nafn);
+    }
+
+    @Test
+    public void testHeimilisfangIsAddress() {
+        assertEquals(vidskiptavinur.getHeimilisfang().get(), heimilisfang);
+    }
+
 }
