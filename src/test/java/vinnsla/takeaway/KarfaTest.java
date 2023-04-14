b0fed75e707e0538712a4f9ba41d260078644a21
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-14 10:41:41 +0000

    Fixed tests comparing int with IntegerProperty

--- src/test/java/vinnsla/takeaway/KarfaTest.java
+++ src/test/java/vinnsla/takeaway/KarfaTest.java
@@ -8,17 +8,14 @@ import static org.junit.Assert.assertEquals;
 
 public class KarfaTest {
     private Karfa karfa;
-
-    /*
-   testa:
-   tóm karfa er tóm
-
-     */
+    private final Veitingar v1 = new Veitingar("First", 1000);
+    private final Veitingar v2 = new Veitingar("Second", 2000);
 
     @Before
-    public void geraNyjaKorfu() {
+    public void geraTomaKorfu() {
         karfa = new Karfa();
     }
+
 
     @Test
     public void testHeildarverdTomKarfa() {
@@ -34,8 +31,47 @@ public class KarfaTest {
     }
 
     @Test
-    public void testHeildarverd() {
+    public void testHeildarverdMedTveimur() {
+        karfa.veitingarList().addAll(v1, v2);
+        assertEquals(karfa.getHeildarverd().get(), v1.getVerd().get() + v2.getVerd().get());
+    }
 
+    @Test
+    public void testHeildarverdRemoveOne() {
+        karfa.veitingarList().addAll(v1, v2);
+        karfa.veitingarList().remove(0);
+        assertEquals(karfa.getHeildarverd().get(), reiknaKarfaHeildarverd());
+    }
+
+    @Test
+    public void testHeildarverdTakaUr() {
+        karfa.veitingarList().addAll(v1, v2);
+        karfa.takaAfMatsedli(0);
+        //karfa.veitingarList().remove(0);
+        assertEquals(karfa.getHeildarverd().get(), reiknaKarfaHeildarverd());
+    }
+
+    @Test
+    public void testHeildarverdClearKarfa() {
+        karfa.veitingarList().addAll(v1, v2);
+        karfa.veitingarList().clear();
+        assertEquals(karfa.getHeildarverd().get(), reiknaKarfaHeildarverd());
+    }
+
+    @Test
+    public void testHeildarverdRemoveAllFromKarfa() {
+        karfa.veitingarList().addAll(v1, v2);
+        karfa.veitingarList().removeAll(v1, v2);
+        assertEquals(karfa.getHeildarverd().get(), reiknaKarfaHeildarverd());
+    }
+
+
+    private int reiknaKarfaHeildarverd() {
+        int reiknadHeildarverd = 0;
+        for (Veitingar v : karfa.veitingarList()) {
+            reiknadHeildarverd += v.getVerd().get();
+        }
+        return reiknadHeildarverd;
     }
 
 }
