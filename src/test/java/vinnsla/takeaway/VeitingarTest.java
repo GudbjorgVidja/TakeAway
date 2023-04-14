5a2823d5184f2016c8840ff7b2693469654d3c1b
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-14 08:31:59 +0000

    Veitingar test case added

--- src/test/java/vinnsla/takeaway/VeitingarTest.java
+++ src/test/java/vinnsla/takeaway/VeitingarTest.java
@@ -1,4 +1,31 @@
 package vinnsla.takeaway;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class VeitingarTest {
+    private Veitingar veitingar;
+    private int verd;
+    private String rettur;
+
+    @Before
+    public void makeVeitingar() {
+        verd = 2500;
+        rettur = "rettur";
+        veitingar = new Veitingar(rettur, verd);
+    }
+
+    @Test
+    public void testVeitingarVerd() {
+        assert (veitingar.getVerd().get() == verd);
+    }
+
+
+
+    /*
+    @Test
+    public void testVeitingarRettur(){
+        //assert (veitingar.get);
+    }
+     */
 }
