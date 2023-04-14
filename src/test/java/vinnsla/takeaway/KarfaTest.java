7b34655f99d66075f3fd166bd9a41d3669f986b9
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-14 08:14:21 +0000

    a couple of tests made for Karfa

--- src/test/java/vinnsla/takeaway/KarfaTest.java
+++ src/test/java/vinnsla/takeaway/KarfaTest.java
@@ -1,6 +1,37 @@
 package vinnsla.takeaway;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class KarfaTest {
-    Karfa karfa = new Karfa();
+    private Karfa karfa;
+
+    /*
+   testa:
+   tóm karfa er tóm
+
+     */
+
+    @Before
+    public void geraNyjaKorfu() {
+        karfa = new Karfa();
+    }
+
+    @Test
+    public void testTomKarfaErTom() {
+        assert (karfa.getObsVeitingar().isEmpty());
+    }
+
+    @Test
+    public void testKarfaMedEinuEkkiTom() {
+        assertFalse(karfa.getObsVeitingar().isEmpty());
+    }
+
+    @Test
+    public void testHeildarverdTomKarfa() {
+        assert (karfa.getHeildarverd().get() == 0);
+    }
 
 }
