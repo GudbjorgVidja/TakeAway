d848e79ac2ac4f3bd4e2908cb75e7bb78e899a22
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-14 08:52:26 +0000

    Made test cases for Matsedill

--- src/test/java/vinnsla/takeaway/MatsedillTest.java
+++ src/test/java/vinnsla/takeaway/MatsedillTest.java
@@ -1,4 +1,46 @@
 package vinnsla.takeaway;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class MatsedillTest {
+    private ObservableList<Veitingar> veitingar;
+    private final Veitingar v1 = new Veitingar("First", 1000);
+    private final Veitingar v2 = new Veitingar("Second", 2000);
+
+    @Before
+    public void initializeEmptyVeitingarList() {
+        veitingar = FXCollections.observableArrayList();
+
+    }
+
+    @Test
+    public void testEmptyListIsEmpty() {
+        assert (veitingar.isEmpty());
+    }
+
+    @Test
+    public void testOneAddedNotEmpty() {
+        veitingar.add(v1);
+        assertFalse(veitingar.isEmpty());
+    }
+
+    @Test
+    public void testFirstAddedIsFirst() {
+        veitingar.addAll(v1, v2);
+        assertEquals(veitingar.get(0), v1);
+    }
+
+    @Test
+    public void testContainsAddedVeitingar() {
+        veitingar.addAll(v1, v2);
+        assertEquals(veitingar.get(0), v1);
+        assertEquals(veitingar.get(1), v2);
+    }
+    
 }
