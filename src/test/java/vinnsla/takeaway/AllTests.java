df2969dda9c974924e33320d3f4639f388acb0ea
KolfinnaK <kolfinnakolbeinsdottir@gmail.com>
2023-04-17 15:51:33 +0000

    Create AllTests.java
    Easy to run all tests at once

---
+++ src/test/java/vinnsla/takeaway/AllTests.java
@@ -0,0 +1,9 @@
+package vinnsla.takeaway;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({KarfaTest.class, MatsedillTest.class, VeitingarTest.class, VidskiptavinurTest.class})
+public class AllTests {
+}
